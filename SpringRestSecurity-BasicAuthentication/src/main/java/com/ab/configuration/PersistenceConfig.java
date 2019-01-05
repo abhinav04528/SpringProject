711ad91da40dd2dae1a2e390f3bb5896cc2b6e6b
Abhinav <abhinav.srivastava04528@gmail.com>
2019-01-06 00:57:01 +0530

    Create Spring Configuration File..

---
+++ SpringRestSecurity-BasicAuthentication/src/main/java/com/ab/configuration/PersistenceConfig.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.ab.configuration;
+
+/**
+ * @author Abhinav
+ *@Date Jan 6, 2019
+ * @Time 12:56:09 AM
+ */
+public class PersistenceConfig {
+
+}
---
+++ SpringRestSecurity-BasicAuthentication/src/main/java/com/ab/configuration/RootConfig.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.ab.configuration;
+
+/**
+ * @author Abhinav
+ *@Date Jan 6, 2019
+ * @Time 12:55:44 AM
+ */
+public class RootConfig {
+
+}
