6d6343e291b7fa6058f8fb7c389648d0ea08b5e6
Abhinav <abhinav.srivastava04528@gmail.com>
2019-01-12 22:39:14 +0530

    Prepare the Root Configuration File commit.

--- SpringRestSecurity-BasicAuthentication/src/main/java/com/ab/configuration/RootConfig.java
+++ SpringRestSecurity-BasicAuthentication/src/main/java/com/ab/configuration/RootConfig.java
@@ -3,11 +3,16 @@
  */
 package com.ab.configuration;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
 /**
  * @author Abhinav
- *@Date Jan 6, 2019
+ * @Date Jan 6, 2019
  * @Time 12:55:44 AM
  */
+@Configuration
+@Import(value = { PersistenceConfig.class })
 public class RootConfig {
 
 }
