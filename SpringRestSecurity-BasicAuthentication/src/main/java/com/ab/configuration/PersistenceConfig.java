0ca7cc30ac17e52f2260463a72705a1a15cb9da4
Abhinav <abhinav.srivastava04528@gmail.com>
2019-01-12 22:34:34 +0530

    Prepare the Configuration File ..

--- SpringRestSecurity-BasicAuthentication/src/main/java/com/ab/configuration/PersistenceConfig.java
+++ SpringRestSecurity-BasicAuthentication/src/main/java/com/ab/configuration/PersistenceConfig.java
@@ -3,11 +3,14 @@
  */
 package com.ab.configuration;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author Abhinav
- *@Date Jan 6, 2019
+ * @Date Jan 6, 2019
  * @Time 12:56:09 AM
  */
+@Configuration
 public class PersistenceConfig {
 
 }
